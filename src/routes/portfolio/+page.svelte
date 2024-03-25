843e5cb80aa798bc46fde1e5e01a90d421fa1f2e
Erik Hartin <erik@nhtbl.studio>
2024-03-25 23:55:09 +0000

    Add portfolio route and query

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { PageData } from './$types'
+  export let data: PageData
+  import PortfolioItem from '$components/PortfolioItem.svelte';
+  const projects = data.data.nhtblProjects.nodes
+  console.log(projects)
+  
+</script>
+
+<ul class="grid grid-cols-2 lg:grid-cols-3 bg-black min-h-screen">
+{#each projects as project}
+<li><PortfolioItem data={project} /></li>
+{/each}
+</ul>
