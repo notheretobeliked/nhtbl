1f31c9f941306a8d5ed96b52180e98331b72c563
Erik Hartin <erik@nhtbl.studio>
2024-04-09 13:54:31 +0100

    video support, intersection observer beginnings

--- src/components/PortfolioItem.svelte
+++ src/components/PortfolioItem.svelte
@@ -1,29 +1,52 @@
 <script lang="ts">
   import { page } from '$app/stores'
-
-  import { pushState } from '$app/navigation'
+  import { onMount } from 'svelte'
 
   import Image from '$components/Image.svelte'
   import Modal from '$components/Modal.svelte' // Assume you have a Modal component for the slideshow
-  import type { PortfolioItemNode, ImageSize } from '$lib/types/wp-types.ts'
+  import type { PortfolioItemNode, ImageSize, MediaNode } from '$lib/types/wp-types.ts'
 
   export let block: PortfolioItemNode
   export let isActive: boolean = false
   export let useHrefs: boolean = false
 
   let showModal: boolean = false
+  let videos: HTMLVideoElement[] = []
 
   const openSlideshow = (event): void => {
-    
     event.preventDefault
-    
-      showModal = true
-    
+
+    showModal = true
   }
 
   const getSrcSet = (sizes: ImageSize[]): string => {
     return sizes.map(({ sourceUrl, width }) => `${sourceUrl} ${width}w`).join(', ')
   }
+  onMount(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            console.log('hit')
+            entry.target.play()
+          } else {
+            entry.target.pause()
+          }
+        })
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
+      },
+    )
+
+    videos.forEach(video => observer.observe(video))
+
+    return () => {
+      observer.disconnect()
+    }
+  })
 </script>
 
 {#if block?.featuredImage?.node?.mediaDetails?.sizes}
@@ -38,7 +61,7 @@
   <Modal on:close={() => (useHrefs ? (location.href = '/portfolio') : (showModal = !showModal))}>
     <h3 class="text-nhtbl-green-base text-center font-display mb-2">{@html block.title}</h3>
     <div class="flex flex-row w-full overflow-x-auto h-[80vh] gap-4">
-        {#each block?.imageGallery?.imageGallery?.nodes as mediaNode}
+      {#each block?.imageGallery?.imageGallery?.nodes || [] as mediaNode}
         {#if mediaNode.mediaType === 'image'}
           <img
             srcset={getSrcSet(mediaNode.mediaDetails.sizes)}
@@ -48,8 +71,8 @@
             class="h-full w-auto max-w-none"
           />
         {:else if mediaNode.mediaType === 'file' && mediaNode.mimeType === 'video/mp4'}
-          <video controls autoplay loop class="h-full w-auto max-w-none">
-            <source src={mediaNode.mediaItemUrl} type="video/mp4">
+          <video bind:this={videos[videos.push() - 1]} controls loop muted class="h-full w-auto max-w-none">
+            <source src={mediaNode.mediaItemUrl} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         {/if}
