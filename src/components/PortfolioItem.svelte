abb35c4757008d35c32076c2d77d319fdc75cc97
Erik Hartin <erik@nhtbl.studio>
2024-04-09 17:10:02 +0100

    hotfix: hover effects on portfolio block

--- src/components/PortfolioItem.svelte
+++ src/components/PortfolioItem.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-
-  import { pushState, goto } from '$app/navigation'
-  import { page } from '$app/stores'
+  import { scale } from 'svelte/transition'
+  import { quintOut } from 'svelte/easing'
   import { onMount } from 'svelte'
   import PortfolioItemDetails from '$components/PortfolioItemDetails.svelte'
   import Image from '$components/Image.svelte'
@@ -11,13 +10,15 @@
   export let block: PortfolioItemNode
   export let isActive: boolean = false
   export let useHrefs: boolean = false
+  export let noLink: boolean = false
 
+  let isHover: boolean = false
   let showModal: boolean = false
   let videos: HTMLVideoElement[] = []
 
   const openSlideshow = (event): void => {
     event.preventDefault()
-    isActive =! isActive
+    if (!noLink) isActive = !isActive
   }
 
   const getSrcSet = (sizes: ImageSize[]): string => {
@@ -52,15 +53,22 @@
 </script>
 
 {#if block?.featuredImage?.node?.mediaDetails?.sizes}
+    
   <a href={block.uri} on:click={openSlideshow} on:keypress={openSlideshow}>
-    <div style="cursor: pointer;">
+    <div class="cursor-pointer relative" on:mouseenter={() =>isHover =! isHover} on:mouseleave={() =>isHover =! isHover}>
       <Image imageSize="medium_large" imageObject={block.featuredImage.node} fit="contain" />
+      {#if isHover && !noLink }
+      <div
+      transition:scale={{ duration: 300, opacity: 0.5, easing: quintOut }} class="bg-nhtbl-green-base p-3 bg-opacity-90 absolute inset-0 flex content-center items-center">
+          <p class="text-black text-xl font-display text-center w-full">{block.title}</p>
+        </div>
+      {/if}
     </div>
   </a>
 {/if}
 
 {#if isActive}
-  <Modal on:close={ () => isActive =! isActive }>
+  <Modal on:close={() => (isActive = !isActive)}>
     <PortfolioItemDetails {block} />
   </Modal>
 {/if}
--- src/components/blocks/PortfolioBlock.svelte
+++ src/components/blocks/PortfolioBlock.svelte
@@ -36,7 +36,7 @@
 
 <div class="bg-black relative -mb-24">
   <Masonry {items} {minColWidth} {maxColWidth} {gap} idKey="slug" let:item animate>
-    <PortfolioItem block={item} />
+    <PortfolioItem block={item} noLink />
   </Masonry>
   <div class="absolute bottom-0 h-[100vh] w-full flex flex-col justify-end bg-gradient-to-t from-black to-transparent">
     <div class="flex justify-center">
