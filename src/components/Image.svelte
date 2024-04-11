d5d965f8527c6c91b2df752c0d6fe2c62899acc3
Erik Hartin <erik@nhtbl.studio>
2024-04-11 23:20:59 +0100

    reduce image sizes

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import type { ImageObject, ImageSize } from '$lib/types/wp-types.ts'
+import type { ImageObject, ImageSize } from '$lib/types/wp-types.ts'
 
   export let imageObject: ImageObject
+
   export let imageSize: 'medium' | 'medium_large' | 'large' | 'full' = 'medium'
   export let fit: "cover" | "contain" | "fill" | "none" = "none"
   export let extraClasses:string = '';
@@ -11,9 +12,9 @@
     return sizes.map(({ sourceUrl, width }) => `${sourceUrl} ${width}w`).join(', ')
   }
 
-  const src = findImageSizeData('sourceUrl', imageObject.mediaDetails.sizes, imageSize) // Note: Ensure the property name matches exactly, JavaScript is case-sensitive.
+  const src = findImageSizeData('sourceUrl', imageObject.mediaDetails.sizes, 'medium_large') // Note: Ensure the property name matches exactly, JavaScript is case-sensitive.
   const width = findImageSizeData('width', imageObject.mediaDetails.sizes, imageSize)
   const height = findImageSizeData('height', imageObject.mediaDetails.sizes, imageSize)
 </script>
 
-<img class="w-full h-full object-{fit} {extraClasses}" {src} alt={imageObject.altText} {width} {height} srcset={getSrcSet(imageObject.mediaDetails.sizes)} sizes="(max-width: 600px) 480px, 800px" />
+<img loading="lazy" class="w-full h-full object-{fit} {extraClasses}" {src} alt={imageObject.altText} {width} {height} srcset={getSrcSet(imageObject.mediaDetails.sizes)} sizes="(max-width: 600px) 480px, 800px" />
