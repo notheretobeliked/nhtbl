54c4efc760db389375cd8b93be1820127c0bdb99
Erik Hartin <erik@nhtbl.studio>
2024-07-11 00:11:11 +0100

    fix: typographic changes to services

--- src/components/blocks/ServicePush.svelte
+++ src/components/blocks/ServicePush.svelte
@@ -2,27 +2,31 @@
   import type { ACFServicePush, ServiceNode } from '$lib/types/wp-types'
   export let block: ACFServicePush
   import BlockRenderer from '$components/BlockRenderer.svelte'
+  import Button from '$components/Button.svelte'
   import Image from '$components/Image.svelte'
   const colour = block.attributes.backgroundColor ?? 'transparent'
 </script>
 
 {#each block.servicePush.service.nodes as serviceBlock}
-  <div class="group relative bg-{colour} transition-colors hover:bg-nhtbl-purple-base duration-300 px-4 py-8 cursor-pointer">
-    <a href={serviceBlock.uri} class="inset-0 flex flex-col md:flex-row gap-4 alignwide ">
+  <div class="relative transition-colors duration-300 px-4 py-8 cursor-pointer">
+    <a href={serviceBlock.uri} class="inset-0 flex flex-col md:flex-row gap-4 alignwide">
       <div class="md:w-[41.66666667%]">
         <figure class="aspect-[3/2] overflow-hidden relative">
           <Image
             imageObject={serviceBlock.featuredImage.node}
-            imageSize='medium'
+            imageSize="medium"
             fit="cover"
             extraClasses="w-full h-full absolute inset-0 transition-transform duration-500 ease-in-out group-hover:scale-105 w-full h-full"
           />
         </figure>
       </div>
-      <div class="transition-colors duration-300 flex flex-col gap-6 w-full">
+      <div class="transition-colors duration-300 flex flex-col gap-3 w-full">
         {#each block.children as block}
           <BlockRenderer {block} forceFull />
         {/each}
+        <div class="flex-shrink mt-4">
+          <Button font="sans" url={serviceBlock.uri} label="Find out more" />
+        </div>
       </div>
     </a>
   </div>
