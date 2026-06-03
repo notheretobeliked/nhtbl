b0cfc39d3cc6c34d6894e77180b8cb794f62dca4
Erik Hartin <erik@nhtbl.studio>
2026-06-03 16:45:23 +0100

    better wide size defaults

--- src/components/BlockRenderer.svelte
+++ src/components/BlockRenderer.svelte
@@ -122,7 +122,7 @@
         baseClasses = 'w-full max-w-full'
         break
       case 'wide':
-        baseClasses = 'w-full max-w-[980px] mx-auto'
+        baseClasses = 'w-full max-w-[1200px] mx-auto'
         break
       case 'none':
         baseClasses = 'w-full max-w-[852px] mx-auto'
